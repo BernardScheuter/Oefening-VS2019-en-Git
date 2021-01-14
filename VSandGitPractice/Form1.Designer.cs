71024998a01e990d90cdb20da8691c28ace733ee
Bernard Scheuter <31876955+BernardScheuter@users.noreply.github.com>
2021-01-14 14:19:30 +0100

    Just a button, loop and timer

---
+++ VSandGitPractice/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace VSandGitPractice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPush = new System.Windows.Forms.Button();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(13, 13);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(147, 44);
+            this.btnPush.TabIndex = 0;
+            this.btnPush.Text = "Push Me!";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCounter.Location = new System.Drawing.Point(221, 13);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(61, 44);
+            this.lblCounter.TabIndex = 1;
+            this.lblCounter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.btnPush);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.Label lblCounter;
+    }
+}
+
---
+++ VSandGitPractice/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VSandGitPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnPush_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                await Task.Delay(1000);
+                lblCounter.Text = Convert.ToString(i);
+            }
+        }
+        // Yes I know that this button can be pressed more than once, but all this async stuff was beyond the scope of this part of the training, I just wanted a timer....
+    }
+}
